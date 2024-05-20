7e2933d056c459b9d7e7353fec48fc13c4a7ce70
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-05-20 15:33:15 -0300

    :lipstick: Style: ajustando width do botão Generate

--- src/pages/Generator/components/Generate/Generate.tsx
+++ src/pages/Generator/components/Generate/Generate.tsx
@@ -19,7 +19,7 @@ const Generate = (props: GenerateProps) => {
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
-                width: '80%',
+                width: '85%',
                 margin: '0 auto',
                 padding: '1rem',
                 backgroundColor: '#A4FFAF',
