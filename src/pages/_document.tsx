20c19afce12c87e1babc6fbd3396898b438ce054
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-05-20 11:15:39 -0300

    :lipstick: style: importando fontes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet" />
       <body>
         <Main />
         <NextScript />
