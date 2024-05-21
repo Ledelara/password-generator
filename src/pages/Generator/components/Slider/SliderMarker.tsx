34f8433c5f3c931d0043680cf1d54a7ecedfe548
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-05-21 10:30:23 -0300

    :recycle: Refactor: calculando número de caracteres que a senha deve ter

--- src/pages/Generator/components/Slider/SliderMarker.tsx
+++ src/pages/Generator/components/Slider/SliderMarker.tsx
@@ -1,69 +1,66 @@
 import { Box, Slider, Typography } from '@mui/material'
-import { useState } from 'react'
 
 const Max = 20
-const Min = 0
+const Min = 1 // Ajustar para garantir que o comprimento mínimo seja 1
+
 const marks = [
-    {
-        value: Min,
-        label: '',
-    },
-    {
-        value: Max,
-        label: '',
-    },
+  {
+    value: Min,
+    label: '',
+  },
+  {
+    value: Max,
+    label: '',
+  },
 ]
 
-const SliderMarker = () => {
-    const [val, setVal] = useState<number>(Min)
+type SliderMarkerProps = {
+  value: number
+  onChange: (value: number) => void
+}
 
-    const handleChange = (event: Event, value: number | number[], activeThumb: number) => {
-        setVal(Number(value))
-    }
+const SliderMarker = ({ value, onChange }: SliderMarkerProps) => {
+  const handleChange = (event: Event, value: number | number[], activeThumb: number) => {
+    onChange(Number(value))
+  }
 
-    return (
-        <Box
-            sx={{ 
-                width: '80%',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-            }}
-        >
-            <Box
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    margin: '0 0 1rem 0',
-                    width: '100%',
-                }}
-            >
-                <Typography
-                    variant="h6"
-                    color="white"
-                >
-                    Comprimento da senha
-                </Typography>
-                <Typography
-                    variant="h6"
-                    color="white"
-                >
-                    {val}
-                </Typography>
-            </Box>
-            <Slider 
-                value={val}
-                marks={marks}
-                min={Min}
-                max={Max}
-                onChange={handleChange}
-                sx={{ 
-                    width: '100%', 
-                    color: 'grey',
-                }}
-            />
-        </Box>
-    )
+  return (
+    <Box
+      sx={{
+        width: '80%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          margin: '0 0 1rem 0',
+          width: '100%',
+        }}
+      >
+        <Typography variant="h6" color="white">
+          Comprimento da senha
+        </Typography>
+        <Typography variant="h6" color="white">
+          {value}
+        </Typography>
+      </Box>
+      <Slider
+        value={value}
+        marks={marks}
+        min={Min}
+        max={Max}
+        onChange={handleChange}
+        sx={{
+          width: '100%',
+          color: 'grey',
+        }}
+      />
+    </Box>
+  )
 }
 
 export default SliderMarker
