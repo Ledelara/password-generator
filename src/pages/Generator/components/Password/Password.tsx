1f4d766eeb4f7db7d4ab102d34680a83ee91f2b8
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-05-21 10:32:58 -0300

    :sparkles: Feat: copiando a senha gerada

--- src/pages/Generator/components/Password/Password.tsx
+++ src/pages/Generator/components/Password/Password.tsx
@@ -1,6 +1,6 @@
 import { FileCopyOutlined } from '@mui/icons-material'
-import { Button, Card, Typography } from '@mui/material'
-import React from 'react'
+import { Button, Card, Tooltip, Typography } from '@mui/material'
+import { useState } from 'react'
 
 type PasswordProps = {
   generatedPassword?: string
@@ -8,34 +8,60 @@ type PasswordProps = {
 
 const Password = (props: PasswordProps) => {
   const { generatedPassword } = props
+  const [tooltipOpen, setTooltipOpen] = useState<boolean>(false)
+
+  const handleCopy = () => {
+    if (generatedPassword) {
+      navigator.clipboard.writeText(generatedPassword)
+        .then(() => {
+          setTooltipOpen(true)
+          setTimeout(() => {
+            setTooltipOpen(false)
+          }, 2000)
+        })
+        .catch((error) => {
+          console.error('Erro ao copiar a senha: ', error)
+        })
+    }
+  }
 
   return (
     <Card
-          sx={{
-            display: 'flex',
-            backgroundColor: '#1a1a1a',
-            width: '30%',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            padding: '1rem',
-          }}
-        >
-        <Typography
-            variant='h4'
-            sx={{
-                color: '#7D7B8C',
-                padding: '0 1rem',
-            }}
-        >
-            {generatedPassword ? generatedPassword : 'S3nH4!'}
-        </Typography>
+      sx={{
+        display: 'flex',
+        backgroundColor: '#1a1a1a',
+        width: '30%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '1rem',
+      }}
+    >
+      <Typography
+        variant='h4'
+        sx={{
+          color: '#7D7B8C',
+          padding: '0 1rem',
+        }}
+      >
+        {generatedPassword ? generatedPassword : 'S3nH4!'}
+      </Typography>
+      <Tooltip
+        title="Copiado!"
+        open={tooltipOpen}
+        disableHoverListener
+        disableFocusListener
+        disableTouchListener
+        placement="top"
+      >
         <Button 
-            startIcon={
-                <FileCopyOutlined 
-                    sx={{ color: '#A4FFAF' }}
-                />
-            }
-        />
+        onClick={handleCopy}
+        startIcon={
+          <FileCopyOutlined 
+            sx={{ color: '#A4FFAF' }}
+          />
+        }
+      />
+      </Tooltip>
     </Card>
   )
 }
